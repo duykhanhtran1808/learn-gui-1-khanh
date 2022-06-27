ae6f9c47d1851ff140760ef04682d3011d32ca52
khanh1 <duykhanhtran1808@gmail.com>
2022-06-27 17:53:19 +0700

    play with grid

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Khanh Company - GUI 1")
-window.minsize(width=800, height=500)
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # label
 
-my_label = tkinter.Label(text="Welcome to the program. My name is Khanh", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="Welcome to the program. My name is Khanh")
+# my_label.pack()
+# my_label.place(x=20, y=20)
+my_label.grid(column=2, row=0)
+# my_label["text"] = "New one"
+# my_label.config(text="Newer one")
+
+status_label = Label(text="The result will be shown here")
+# status_label.pack()
+status_label.grid(column=2, row=4)
+
+# entry
+
+input_field = Entry(width=10)
+# input_field.pack()
+input_field.grid(column=0, row=2)
+
+# button
+
+
+def button_clicked():
+    input_result = input_field.get()
+    status_label.config(text=input_result)
+
+
+button = Button(text="Hit now", command=button_clicked)
+# button.pack()
+button.grid(column=1, row=3)
 
 # experiment
 
--- playground.py
+++ playground.py
@@ -7,3 +7,24 @@ def add(*args):
 
 result = add(1, 2, 3, 4, 5)
 print(result)
+
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+
+
+my_car = Car(model="G6")
+print(my_car.make)
