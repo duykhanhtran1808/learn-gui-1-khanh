f3ae22b0907fa3f98479e01365997f163bfa11e4
khanh1 <duykhanhtran1808@gmail.com>
2022-06-27 16:13:45 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Khanh Company - GUI 1")
+window.minsize(width=800, height=500)
+
+# label
+
+my_label = tkinter.Label(text="Welcome to the program. My name is Khanh", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+
+window.mainloop()
